b6fb3fb568896d57dec6ed8925c49f61e0f06ac8
BrasD99 <brasd99@gmail.com>
2023-08-28 14:21:18 +0300

    added favicon and enter click handler on home page

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -9,3 +9,12 @@ searchButton.addEventListener('click', function() {
   const url = `${protocol}//${currentHost}${endpoint}?query=${encodeURIComponent(inputValue)}`;
   window.location.href = url;
 });
+
+searchTerm.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) { // 13 represents the Enter key
+    event.preventDefault(); // Prevent form submission
+
+    // Trigger the click event on the search button
+    searchButton.click();
+  }
+});
